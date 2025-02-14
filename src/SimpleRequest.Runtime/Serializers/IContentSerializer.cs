3e059bb507026993b7cf51a1eb9e065dc9adc939
Ian Johnson <ianjohnson@ians-mbp.mynetworksettings.com>
2025-02-14 08:26:49 -0500

    updating serializer to have serialize and desialize string methods

--- src/SimpleRequest.Runtime/Serializers/IContentSerializer.cs
+++ src/SimpleRequest.Runtime/Serializers/IContentSerializer.cs
@@ -9,7 +9,13 @@ public interface IContentSerializer {
     
     Task Serialize(Stream stream, object value, CancellationToken cancellationToken = default);
     
+    string Serialize(object value);
+    
     ValueTask<object?> Deserialize(Stream stream, Type type, CancellationToken cancellationToken = default);
     
     ValueTask<T?> Deserialize<T>(Stream stream, CancellationToken cancellationToken = default);
+    
+    object? Deserialize(string stringValue, Type type);
+    
+    T? Deserialize<T>(string stringValue);
 }
--- src/SimpleRequest.Runtime/Serializers/SystemTextJsonSerializer.cs
+++ src/SimpleRequest.Runtime/Serializers/SystemTextJsonSerializer.cs
@@ -39,11 +39,23 @@ public class SystemTextJsonSerializer : IContentSerializer {
         return JsonSerializer.SerializeAsync(stream, value, _options, cancellationToken);
     }
 
+    public string Serialize(object value) {
+        return JsonSerializer.Serialize(value, _options);
+    }
+
     public ValueTask<object?> Deserialize(Stream stream, Type type, CancellationToken cancellationToken = default) {
         return JsonSerializer.DeserializeAsync(stream, type, _options, cancellationToken);
     }
 
     public ValueTask<T?> Deserialize<T>(Stream stream, CancellationToken cancellationToken = default) {
         return JsonSerializer.DeserializeAsync<T>(stream, _options, cancellationToken);
+    }
+
+    public object? Deserialize(string stringValue, Type type) {
+        return JsonSerializer.Deserialize(stringValue, type, _options);
+    }
+
+    public T? Deserialize<T>(string stringValue) {
+        return JsonSerializer.Deserialize<T>(stringValue, _options);
     }
 }
