3fdbd91c9db5a55e16bcf999ed0407399c9be5ac
Ian Johnson <ianjohnson@ians-mbp.mynetworksettings.com>
2025-03-11 21:45:59 -0400

    fixing custom attribute name problem

--- src/SimpleRequest.Runtime/Serializers/ParameterBindingService.cs
+++ src/SimpleRequest.Runtime/Serializers/ParameterBindingService.cs
@@ -71,7 +71,7 @@ public class ParameterBindingService(IContentSerializerManager contentSerializer
                 var parameterValue = await provider.GetParameterValueAsync(context, parameterInfo);
 
                 if (parameterValue != null) {
-                    parameters.Set(parameterInfo.Name, parameterInfo.Index);
+                    parameters.Set(parameterValue, parameterInfo.Index);
                     return;
                 }
             }
